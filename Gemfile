28dc02620ced69eb3d20aedbb3c732382722344f
Lance Ennen <byennen@gmail.com>
2011-06-13 21:12:30 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'unicorn'
+gem 'spree', :git => 'git://github.com/spree/spree.git'
+
+group :production do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeSandbox::Application.config.secret_token = 'fe705d7859bf3a80d5b576eea7a8e1cc9739ca5b5cc72b5e4ad7361da36224ef722dfb9a68bd74f250006e9f14f131e0ed49255dfc010e2927336e806075f6b2'
